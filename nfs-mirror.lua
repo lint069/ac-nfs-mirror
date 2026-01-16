764b38364d0b8d35071b3c9581f21eb5934d1d4c
lint069 <ljovic009@gmail.com>
2026-01-16 01:25:34 +0100

    feat: add app scaling and refine about section(settings)

--- nfs-mirror.lua
+++ nfs-mirror.lua
@@ -46,16 +46,23 @@ local trackFolderName = ac.getTrackID()
 
 --#region helper functions
 
+---Scales input value by the app scale.
+---@param value number
+---@return number @Scaled value.
+local function scale(value)
+    return math.floor(value * app.scale)
+end
+
 ---@param text string @Text to be displayed in the tooltip.
----@param cursorType? ui.MouseCursor @Changes the mouse cursor to
+---@param cursorType? ui.MouseCursor @Changes the mouse cursor to given cursorType.
 local function tooltip(text, cursorType)
     if ui.itemHovered() then
         if cursorType then ui.setMouseCursor(cursorType) end
-        ui.tooltip(vec2(8, 4), function() ui.text(text) end)
+        ui.tooltip(vec2(7, 4), function() ui.text(text) end)
     end
 end
 
----converts degrees to radians
+---Converts degrees to radians.
 ---@param deg number
 local function degToRad(deg)
     return deg * math.pi / 180
@@ -63,7 +70,7 @@ end
 
 --#endregion
 
---#region ロジック関数
+--#region logic functions
 
 local appWindow = ac.accessAppWindow('IMGUI_LUA_NFS mirror_main')
 
@@ -104,25 +111,26 @@ end
 --#region drawing functions
 
 local function drawMirror()
-    local virtualMirrorPos = vec2(15, 15)
-    local virtualMirrorSize = vec2(470, 140)
+    local virtualMirrorPos = vec2(15, 15):scale(app.scale)
+    local virtualMirrorSize = vec2(470, 140):scale(app.scale)
+    local whitePoint = 1.3 - (sim.lightSuggestion * 0.5)
 
     ui.beginTonemapping()
     ui.drawVirtualMirror(virtualMirrorPos, virtualMirrorPos + virtualMirrorSize)
-    ui.endTonemapping(1, 1.8 - (sim.lightSuggestion * 0.8), true) --FIXME: !?
+    ui.endTonemapping(1, whitePoint, true)
 
     local mirrorPos = vec2(0, 0)
-    local mirrorSize = vec2(500, 193)
+    local mirrorSize = vec2(500, 193):scale(app.scale)
 
     ui.drawImage(app.images.mirror, mirrorPos, mirrorPos + mirrorSize, rgbm.colors.white)
 end
 
 local function drawRing()
-    local center = vec2(250, 183)
+    local center = vec2(250, 183):scale(app.scale)
     local arcStart, arcEnd = degToRad(180), degToRad(360)
-    local radiusInner, radiusOuter = 16, 26
+    local radiusInner, radiusOuter = scale(16), scale(26)
     local segments = 22
-    local arcThickness = 4
+    local arcThickness = scale(4)
 
     ui.pathArcTo(center, radiusInner, arcStart, arcEnd, 4)
     ui.pathFillConvex(colors.circleFill)
@@ -183,7 +191,7 @@ end
 
 --#region settings window
 
-local manifest = ac.INIConfig.load(ac.getFolder(ac.FolderID.ACAppsLua) .. '/nfs_mirror/manifest.ini', ac.INIFormat.Extended)
+local manifest = ac.INIConfig.load(ac.getFolder(ac.FolderID.ACAppsLua) .. '/nfs-mirror/manifest.ini', ac.INIFormat.Extended)
 local appVersion = manifest:get('ABOUT', 'VERSION', 0.00)
 
 function script.settings()
@@ -192,18 +200,18 @@ function script.settings()
         colors.userAccentColor = ac.getUI().accentColor
     end
 
-    local firstItemOffset = 5
-
     ui.tabBar('settings', function()
         ui.tabItem('App', function()
-            ui.offsetCursorY(firstItemOffset)
+            app.scale = ui.slider('##', app.scale, 0.5, 1.5, 'App Scale: %.1f')
+			if ui.itemHovered() and ui.mouseReleased(ui.MouseButton.Right) then app.scale = 1 end
+			tooltip('Right-click to reset.', ui.MouseCursor.ResizeEW)
 
 			if ui.checkbox('Force App to Center', settings.centerApp) then settings.centerApp = not settings.centerApp end
 
             if not isOnlineRace then
                 ui.separator()
 
-                ui.textDisabled('More options are only avalible for an online race.')
+                ui.textDisabled('More options only avalible in online race.')
                 return
             end
 
@@ -243,22 +251,33 @@ function script.settings()
         end)
 
         ui.tabItem('About', function()
-            ui.offsetCursorY(firstItemOffset)
-
 			ui.text('v' .. appVersion)
 
-            ui.separator()
+            ui.sameLine(0, 4)
 
-            ui.newLine(-12)
+            ui.text('– Licensed under the')
+
+            ui.sameLine(0, 4)
+
+            local licenseInfo = 'https://opensource.org/licenses/MIT'
+
+            ui.textHyperlink('MIT License')
+            if ui.itemHovered() and ui.mouseReleased(ui.MouseButton.Left) then os.openURL(licenseInfo, true) end
+
+            ui.separator()
+            ui.newLine(-5)
+
+            ui.text('Encounter any bugs?')
 
             ui.pushStyleColor(ui.StyleColor.Button, colors.issueButton.idle)
             ui.pushStyleColor(ui.StyleColor.ButtonHovered, colors.issueButton.hovered)
             ui.pushStyleColor(ui.StyleColor.ButtonActive, colors.issueButton.active)
 
-            local issueUrl = 'https://github.com/lint069/ac-nfs-mirror/issues/new'
+            local issueUrl = 'https://github.com/lint069/ac-nfs-mirror/issues/new' .. '?template=bug_report.yml'
 
-            if ui.button('Report Issue', ui.ButtonFlags.None) then os.openURL(issueUrl, false) end
-            tooltip('Requires you to be logged into your github account.\nAlternatively you can message me on discord @wallpaperengineman', ui.MouseCursor.Hand)
+            ui.setCursor(vec2(160, 85))
+            if ui.button('Report Issue') then os.openURL(issueUrl, true) end
+            tooltip('Requires a GitHub account.\nAlternatively, contact me on Discord: @wallpaperengineman', ui.MouseCursor.Hand)
 
             ui.popStyleColor(3)
         end)
@@ -274,4 +293,5 @@ function script.windowMain()
     drawMirror()
     drawRing()
     drawArrow()
+    ui.setCursor(vec2(500, 193):scale(app.scale)) --temporary. AUTO_RESIZE is not working?
 end
